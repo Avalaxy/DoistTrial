8622745b769fcb0aa3d308a6e7abf74696323701
Leon Cullens <leoncullens@gmail.com>
2016-01-10 18:43:37 +0100

    Add TaskFilter class.

---
+++ Filters/TaskFilter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Filters.Models;
+
+namespace Filters
+{
+    public class TaskFilter
+    {
+        public Section FilterItems(IEnumerable<Item> items, string query)
+        {
+            var results = new Section();
+
+            return results;
+        } 
+    }
+}
